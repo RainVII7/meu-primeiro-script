fcd17f04f1d699e2397c05db21ec1017eabeb8e6
RainVII7 <licksvinnie@gmail.com>
2026-06-09 22:33:17 -0300

    Update script to use new calculator file

--- meu_primeiro_script.sh
+++ meu_primeiro_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash/
 echo "===> Script Iniciado <==="
 
-python3 /home/user/python/minha_calculadora_mestre.py
+python3 /home/user/python/minha_primeira_calculadora_em_python.py
 # Substitua o 'user' pelo seu nome de usuário
 
 echo "===> Finalizando Script <==="
