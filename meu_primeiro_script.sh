6fe126870f77a907ba8f735254bc44b98fcd8671
RainVII7 <licksvinnie@gmail.com>
2026-06-09 22:03:57 -0300

    Update meu_primeiro_script.sh

--- meu_primeiro_script.sh
+++ meu_primeiro_script.sh
@@ -1,18 +1,7 @@
-!#/bin/bash/
+#!/bin/bash/
 echo "===> Script Iniciado <==="
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+python3 /home/user/modulo1/linux/script/python/minha_calculadora_mestre.py
+# Substitua o 'user' pelo seu nome de usuário
 
 echo "===> Finalizando Script <==="
